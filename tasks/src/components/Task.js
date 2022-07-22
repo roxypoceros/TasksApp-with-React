6f7ea6a50254c9a8b71b304c2f111295bf89e4c3
Roxy Poceros <roxypoceros@gmail.com>
2022-07-21 23:59:14 -0500

    Cambios en App

--- tasks/src/components/Task.js
+++ tasks/src/components/Task.js
@@ -2,15 +2,20 @@ import React from "react"
 import "../styles/Task.css"
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Task({ text, completed }) {
+function Task({ id, text, completed, completeTask, deleteTask }) {
   return (
     /* A continuación asignar nombre de clase condicional  */
     <div className={completed ? "taskContainer completed" : "taskContainer"}>
 
-      <div className="taskText">
+      <div className="taskText"
+      /* onClick necesita que su valor sea una función.
+      Función anómima se va a ejecutar y va a llamar a completeTask (pasada como prop)*/
+      onClick={() => completeTask(id)}>
+        
         {text}
       </div>
-      <div className="taskContainerIcons">
+      <div className="taskContainerIcons"
+      onClick={() => deleteTask(id)}>
         {/* Se le puede agregar al ícono una clase, 
         se le pasa como prop */}
         <AiOutlineCloseCircle className="iconTask" />
---
+++ tasks/src/components/TasksList.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import TaskForm from './TaskForm'
+import Task from './Task';
+import "../styles/TasksList.css"
+
+function TasksList() {
+  /* USESTATE usamos una constante y un arreglo con dos elementos, 
+  El estado que queremos mantener y 
+  La función que vamos a usar para actualizar ese estado
+  Inicialmente "task" será un arreglo vacío y se marca después de useState
+    */
+   
+  const [tasks, setTasks] = useState([]);
+  /* Lo siguiente es la lista del componente en jsx */
+  return (
+    <>
+      <TaskForm /> 
+      {/* El sig div va a contener una lista de componentes */} 
+      <div className='tasksListContainer'>
+        {/* para renderizar una lista de componentes Task, 
+        c/u con sus propios valores para props.
+        1o. Uso llaves porque voy a usar un método de arreglos de js.
+        2o. Tomamos el arreglo "tasks" que definí en useState como estado 
+        que va a contener todas las tareas y llamo función .map.
+        .MAP toma c/u de las tareas las pasa 1 por 1 como el argumento de otra función
+        realiza lo que especifiquemos con esa tarea. 
+        ej. Crear un componente Tarea para cada una de las tareas en el arreglo
+        */}
+        {
+          tasks.map((task) =>
+          <Task 
+          texto={task.texto}
+          completed={task.completed}
+          />
+          )
+        }
+      </div>
+    </>
+  )
+}
+
+export default TasksList
